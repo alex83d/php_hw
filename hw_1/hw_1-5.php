8971ec1dd13326b3091ff9664d251eb3dffed6f0
alex <60008473+alex83d@users.noreply.github.com>
2021-11-17 12:46:19 +0300

    first

--- hw_1/hw_1-5.php
+++ hw_1/hw_1-5.php
@@ -1,1 +1,54 @@
 <?php
+/*$bmw = [
+    'model' => "X5",
+    'speed' => 120,
+    'doors' => 5,
+    'year' => "2015"
+];
+$toyota = [
+    'model' => "X5",
+    'speed' => 150,
+    'doors' => 5,
+    'year' => "2006"
+];
+$opel = [
+    'model' => "X5",
+    'speed' => 200,
+    'doors' => 5,
+    'year' => "2017"
+];
+function fun(...$name): array
+{
+    return [...$name];
+}
+
+$car = array_map('fun', $bmw, $toyota, $opel);
+print_r($car);*/
+$cars = [
+    ['name' => "bmw",
+        'model' => "X5",
+        'speed' => 120,
+        'doors' => 5,
+        'year' => "2015"
+    ],
+    [
+        'name' => "toyota",
+        'model' => "X5",
+        'speed' => 150,
+        'doors' => 4,
+        'year' => "2006"
+    ],
+    ['name' => "opel",
+        'model' => "X5",
+        'speed' => 200,
+        'doors' => 4,
+        'year' => "2017"
+    ]
+];
+
+foreach ($cars as $key => $value) {
+    echo '<pre>' . PHP_EOL;
+    print_r("CAR " . $value['name'] . '<br>' . " " . $value['model']
+        . " " . $value['speed'] . " "
+        . $value['doors'] . " " . $value['year'] . PHP_EOL);
+}
--- hw_1/table.php
+++ hw_1/table.php
@@ -2,15 +2,47 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <style>
+        table {
+            width: 100%;
+            margin: 0 auto;
+        }
+        table,
+        td {
+            border: 1px solid #333;
+            text-align: center
+        }
+        tr {
+            background: dimgrey;
+        }
+    </style>
     <title>Table</title>
 </head>
 <body>
 <table>
-<?php
-for ($i=1; $i < 10; $i++) {
-echo "<tr>";
-}
-?>
+
+    <?php
+    $table = '<th>  </th>';
+    for ($i = 1; $i <= 10; $i++) {
+        $table .= "<th>$i</th>";
+    }
+
+    for ($i = 1; $i <= 10; $i++) {
+        $table .= "<tr><td><strong>$i</strong></td>";
+        for ($j = 1; $j <= 10; $j++) {
+            $result = $i * $j;
+            if ($i % 2 == 0 && $j % 2 == 0) {
+                $result = "($result)";
+            } else {
+                $result = "[$result]";
+            }
+            $table .= '<td>' . $result . '</td>';
+        }
+        $table .= '</tr>';
+    }
+
+    print $table;
+    ?>
 
 </table>
 
